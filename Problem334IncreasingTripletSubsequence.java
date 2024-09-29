04eccda4a3bafec66dbf9264db5fef6be3051613
Namit Sharma <vats.namit@gmail.com>
2024-09-29 06:07:19 +0530

    Create Problem334IncreasingTripletSubsequence.java
    Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,4,5]
    Output: true
    Explanation: Any triplet where i < j < k is valid.
    Example 2:
    
    Input: nums = [5,4,3,2,1]
    Output: false
    Explanation: No triplet exists.
    Example 3:
    
    Input: nums = [2,1,5,0,4,6]
    Output: true
    Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
     
    
    Constraints:
    
    1 <= nums.length <= 5 * 105
    -231 <= nums[i] <= 231 - 1
     
    
    Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?

---
+++ Problem334IncreasingTripletSubsequence.java
@@ -0,0 +1,23 @@
+class Problem334IncreasingTripletSubsequence {
+
+    public boolean increasingTriplet(int[] nums) {
+
+        if (nums.length <= 2)
+            return false;
+        
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;;
+
+        for (int i = 0; i< nums.length; i++) {
+            if (nums[i] <= a) {
+                a = nums[i];
+            } else if (nums[i] <= b) {
+                b = nums[i];
+            } else {
+                return true; // means we found a 'c' greater than a and b
+            }
+        }
+
+        return false;
+    }
+}
