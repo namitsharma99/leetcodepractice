b2ee2636ba55069ec54da202a9c784ffe9b4ab11
Namit Sharma <vats.namit@gmail.com>
2024-12-18 04:07:56 +0530

    Create 872LeafSimilarTrees.java
    Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
    
    
    
    For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
    
    Two binary trees are considered leaf-similar if their leaf value sequence is the same.
    
    Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
    
     
    
    Example 1:
    
    
    Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
    Output: true
    Example 2:
    
    
    Input: root1 = [1,2,3], root2 = [1,3,2]
    Output: false
     
    
    Constraints:
    
    The number of nodes in each tree will be in the range [1, 200].
    Both of the given trees will have values in the range [0, 200].

---
+++ 872LeafSimilarTrees.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class 872LeafSimilarTrees {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        
+        List<Integer> l1 = new ArrayList();
+        sequence(l1, root1);
+        //System.out.println(l1);
+
+        List<Integer> l2 = new ArrayList();
+        sequence(l2, root2);
+        //System.out.println(l2);
+
+        if (l1.size() != l2.size()) {
+            return false; 
+        } else {
+            //System.out.println("same size --- ");
+            for (int i = 0; i< l1.size(); i++) {
+                if (l1.get(i) - l2.get(i) != 0) {
+                    //System.out.println("mismatch === " +  l1.get(i)  + " ;;; " + l2.get(i) );
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    
+    }
+
+    private void sequence (List<Integer> ls, TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+        
+        if (root.left == null && root.right == null) {
+            // str = String.valueOf(root.val) + ",";
+            ls.add(root.val);
+        } else {
+            // str = String.valueOf(sequence(root.left)) + String.valueOf(sequence(root.right));
+            sequence(ls, root.left);
+            sequence(ls, root.right);
+        } 
+
+    }
+}
