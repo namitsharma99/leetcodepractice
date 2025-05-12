b526f2d1db6d1ea39c119b9311cb69db22f7782e
Namit Sharma <vats.namit@gmail.com>
2025-05-13 00:59:16 +0530

    Update Problem55JumpGame.java

--- Problem55JumpGame.java
+++ Problem55JumpGame.java
@@ -1,17 +1,37 @@
+// class Problem55JumpGame {
+//     public boolean canJump(int[] nums) {
+//        int ptr = 0;
+//        for(int i = 0; i < nums.length; i++) {
+//             System.out.println("i: " + i + ", num: " + nums[i] + ", ptr: " + ptr);
+//             if(i > ptr) {
+//                 System.out.println("Returning false!");
+//                 return false;
+//             }
+//             int jump = i + nums[i];
+//             ptr = Math.max(ptr, jump);
+//             System.out.println("jump: " + jump + ", i: " + i + ", ptr: " + ptr);
+//             System.out.println("-------------------------------------------");
+//        } 
+//        return true;
+//     }
+// }
+
 class Problem55JumpGame {
     public boolean canJump(int[] nums) {
-       int ptr = 0;
-       for(int i = 0; i < nums.length; i++) {
-            System.out.println("i: " + i + ", num: " + nums[i] + ", ptr: " + ptr);
-            if(i > ptr) {
-                System.out.println("Returning false!");
+        
+        int count = 0;
+
+        for (int i = 0; i< nums.length; i++) {
+            if (i > count)
                 return false;
-            }
-            int jump = i + nums[i];
-            ptr = Math.max(ptr, jump);
-            System.out.println("jump: " + jump + ", i: " + i + ", ptr: " + ptr);
-            System.out.println("-------------------------------------------");
-       } 
-       return true;
+
+            count = Math.max(count, i + nums[i]);
+
+            if (count >= nums.length - 1)
+                return true;
+        }
+
+        return true;
+
     }
 }
