35347388abc737bfff2606372e8713a9518753b0
Namit Sharma <vats.namit@gmail.com>
2025-10-09 08:20:06 +0530

    Create Problem704BinarySearch.java
    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
    
    You must write an algorithm with O(log n) runtime complexity.
    
     
    
    Example 1:
    
    Input: nums = [-1,0,3,5,9,12], target = 9
    Output: 4
    Explanation: 9 exists in nums and its index is 4
    Example 2:
    
    Input: nums = [-1,0,3,5,9,12], target = 2
    Output: -1
    Explanation: 2 does not exist in nums so return -1
     
    
    Constraints:
    
    1 <= nums.length <= 104
    -104 < nums[i], target < 104
    All the integers in nums are unique.
    nums is sorted in ascending order.

---
+++ Problem704BinarySearch.java
@@ -0,0 +1,23 @@
+class Problem704BinarySearch {
+
+    public int search(int[] nums, int target) {
+        
+        int low = 0, high = nums.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = low + (high-low)/2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[mid] > target) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return -1;
+
+    }
+}
