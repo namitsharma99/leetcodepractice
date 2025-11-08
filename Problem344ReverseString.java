f7e0fd7f3d2b68ef8db688b2d7383c2870ab4ebd
Namit Sharma <vats.namit@gmail.com>
2025-11-09 00:19:41 +0530

    Create Problem344ReverseString.java
    Write a function that reverses a string. The input string is given as an array of characters s.
    
    You must do this by modifying the input array in-place with O(1) extra memory.
    
     
    
    Example 1:
    
    Input: s = ["h","e","l","l","o"]
    Output: ["o","l","l","e","h"]
    Example 2:
    
    Input: s = ["H","a","n","n","a","h"]
    Output: ["h","a","n","n","a","H"]
     
    
    Constraints:
    
    1 <= s.length <= 105
    s[i] is a printable ascii character.

---
+++ Problem344ReverseString.java
@@ -0,0 +1,18 @@
+class Problem344ReverseString {
+
+    public void reverseString(char[] s) {
+        
+        Stack<Character> stack = new Stack();
+
+        for (char c: s) {
+            stack.push(c);
+        }
+
+        int i = 0;
+
+        while (!stack.isEmpty()) {
+            s[i++] = stack.pop();
+        }
+    }
+    
+}
