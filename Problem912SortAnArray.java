471d55a88f6a66cbe89f9107811d1ef879e5ee3d
Namit Sharma <vats.namit@gmail.com>
2025-11-10 23:24:03 +0530

    Create Problem912SortAnArray.java
    Given an array of integers nums, sort the array in ascending order and return it.
    
    You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
    
     
    
    Example 1:
    
    Input: nums = [5,2,3,1]
    Output: [1,2,3,5]
    Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
    Example 2:
    
    Input: nums = [5,1,1,2,0,0]
    Output: [0,0,1,1,2,5]
    Explanation: Note that the values of nums are not necessarily unique.
     
    
    Constraints:
    
    1 <= nums.length <= 5 * 104
    -5 * 104 <= nums[i] <= 5 * 104

---
+++ Problem912SortAnArray.java
@@ -0,0 +1,66 @@
+class Problem912SortAnArray {
+
+    public int[] sortArray(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+
+        mergeSort(nums, low, high);
+
+        return nums;
+        
+    }
+
+    private void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = low + (high-low)/2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    private void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[low + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+
+        int k = low;
+
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+
+    }
+}
